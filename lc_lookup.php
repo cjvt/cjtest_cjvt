396bcd9bb51fb939bc39348a025595b401ee9e14
cjvt <chenjie709@gmail.com>
2014-07-28 17:43:07 -0400

    modify lc_lookup.php

---
+++ lc_lookup.php
@@ -0,0 +1,43 @@
+<?php
+
+$call = $argv[1];
+
+require_once(dirname(__FILE__) . '/lib_addison.php');
+
+debug("Getting Addison record");
+
+$bib = library_bib_from_call($call);
+if (!$bib)
+{
+	debug("Couldn't retrieve bib number.  Aborting.");
+	return(false);
+}
+debug("Bib:      $bib");
+
+// get the full catalog record, and then filter it into a smaller array
+
+$xrecord = library_xrecord_from_bib($bib);	
+$GLOBALS['last_xrecord'] = $xrecord;
+
+if (!$xrecord)
+{
+	debug("Couldn't retrieve xrecord.  Aborting.");
+	return(false);
+}
+
+$simple = simple_array_from_xrecord($xrecord);
+
+foreach ($simple as $k => $v)
+{
+	foreach ($v as $vx)
+	{
+		echo str_pad($k . ':', 10, ' ', STR_PAD_RIGHT) . $vx . "\n";
+	}
+}
+
+function debug($text)
+{
+	echo "$text\n";
+}
+
+//git test
